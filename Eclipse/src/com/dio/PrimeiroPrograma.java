d1da07f7e6beb8efa3eaf0086d3cc199b8d7159a
brunacr <bruna.leitao.pro@gmail.com>
2022-07-11 18:07:26 -0300

    Projeto com Eclipse

---
+++ Eclipse/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*int a = 2;
+		int b= 3;
+		System.out.println("Hello, world! " + (a+b));*/
+
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String numPag;
+}
