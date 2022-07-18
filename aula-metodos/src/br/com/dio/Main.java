e86d35feafcf94bcba40435cc4b03acbbe3d0fc9
brunacr <bruna.leitao.pro@gmail.com>
2022-07-18 17:44:07 -0300

    Aula Métodos

---
+++ aula-metodos/src/br/com/dio/Main.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+//Classe de exemplo para o exercício da aula 01 de métodos
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Calculadora
+		System.out.println("Exercício calculadora");
+		Calculadora.soma(3, 6);
+		Calculadora.subtracao(9, 1.8);
+		Calculadora.multiplicacao(7, 8);
+		Calculadora.divisao(5, 2.5);
+
+		//Mensagem
+		System.out.println("Exercício calculadora");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(1);
+
+		//Emprestimo
+		System.out.println("Exercício empréstimo");
+		Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+		Emprestimo.calcular(1000, 5);
+	}
+	
+}
